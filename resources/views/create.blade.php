70558f08391d1d896dc2b01407b8d7fdaaed8f09
kk-0329 <3213.kotaro.basketball@gmail.com>
2022-03-22 01:49:36 +0000

    06 use css framework

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,48 +1,34 @@
-<h1>New Products</h1>
+@extends('layouts.app')
 
-<form method="POST" action="/products">
-    {{ csrf_field() }}
-    <div class="form-group">
-        <label for="name">商品名</label>
-        <input type="text" name="name">
+@section('content')
+    <div class="container">
+        <h1>新しい商品を追加</h1>
+
+        <form method="POST" action="/products">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="product-name">商品名</label>
+                <input type="text" name="name" id="product-name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="product-description">商品説明</label>
+                <textarea name="description" id="product-description" class="form-control"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="product-price">価格</label>
+                <input type="number" name="price" id="product-price" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="product-category">カテゴリ</label>
+                <select name="category_id" class="form-control" id="product-category">
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <button type="submit" class="btn btn-success">商品を登録</button>
+        </form>
+
+        <a href="/products">商品一覧に戻る</a>
     </div>
-    <div class="form-group">
-        <label for="description">商品概要</label>
-        <textarea name="description" cols="30" rows="10"></textarea>
-    </div>
-    <div class="form-group">
-        <label for="number">値段</label>
-        <input type="number" name="price">
-    </div>
-    <select name="category_id" class="form-group">
-        @foreach($categories as $category)
-            <option value="{{ $category->id }}">{{ $category->name }}</option>
-        @endforeach
-    </select>
-    <button type="submit">新規追加</button>
-</form>
-
-<a href="/products">戻る</a>
-
-{{--<h1>New Products</h1>--}}
-
-{{--{{ Form::open(['route' => 'products.create']) }}--}}
-{{--    {{Form::token()}}--}}
-{{--    <div class="form-group">--}}
-{{--        {{ Form::label('name', '商品名:') }}--}}
-{{--        {{ Form::text('name', null) }}--}}
-{{--    </div>--}}
-{{--    <div class="form-group">--}}
-{{--        {{ Form::label('description', '商品概要') }}--}}
-{{--        {{ Form::text('description', null) }}--}}
-{{--    </div>--}}
-{{--    <div class="form-group">--}}
-{{--        {{ Form::label('number', '値段') }}--}}
-{{--        {{ Form::number('number', null) }}--}}
-{{--    </div>--}}
-{{--    <div class="form-group">--}}
-{{--        {{ Form::submit('新規追加', ['class' => 'btn btn-outline-primary']) }}--}}
-{{--    </div>--}}
-{{--{{ Form::close() }}--}}
-
-{{--<a href="/products">Back</a>--}}
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://kit.fontawesome.com/cdcd15bae3.js" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,7 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/laravel-ec.css') }}">
+    <link href="{{ asset('css/laravel-ec.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -73,7 +74,7 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="py-4 mb-5">
             @yield('content')
         </main>
     </div>
